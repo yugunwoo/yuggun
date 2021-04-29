cccda7caac2f12ce8095dd6abec97fc84def0601
yugunwoo <45531840+yugunwoo@users.noreply.github.com>
2021-04-29 21:04:50 +0900

    commit

---
+++ 4.29.py
@@ -0,0 +1,12 @@
+numbers = [273,103,5,32,65,9,72,800,99]
+'''for i in numbers
+    if i>=100:
+        print("100이상의수: {}".format(i))'''
+
+for i in numbers:
+    if(i%2==0):
+        print("짝수:{}".format(i))
+    else:
+        print("홀수:{}".format(i))
+
+
